a33ad859bb0ee449d2246803782df24029cd4be1
Göcs Jëss <laurengocs.1@gmail.com>
2017-11-29 16:12:27 +0800

    main content added

---
+++ main.js
@@ -0,0 +1,39 @@
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const token = '';
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '~ping') {
+        message.channel.send('~pong');
+    }
+    if (message.content === '~avatar') {
+        message.reply(message.author.avatarURL);
+    }
+    if (message.content === '~home') {
+        message.react("🇵🇭");
+    }
+    if (message.content === '~reace') {
+        const collector = message.createReactionCollector(
+            ( reaction, user) => reaction.emoji.name === '👌' && user.id === 'someID',
+            { time: 15000 }
+        );
+        collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+        collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+    }
+});
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find('name', 'member-log');
+    if (!channel) return;
+    channel.send(`Welcome to the server, ${member}`);
+});
+
+// Log our bot in
+client.login(token);
